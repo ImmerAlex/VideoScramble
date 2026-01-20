338b484cbd45149a27ab13ae3c24ebad52fd8650
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2026-01-20 09:40:47 +0100

    build: ajout usage help

--- src/main/java/fr/aimmer/Main.java
+++ src/main/java/fr/aimmer/Main.java
@@ -8,7 +8,14 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		if (args.length != 3) throw new RuntimeException("Invalid number of arguments");
+		if (args.length != 3) {
+			System.out.println("""
+			Chiffrement et déchiffrement de vidéo.
+
+			Usage: 
+				java -jar video-scramble.jar <width> <height> file""");
+			System.exit(1);
+		}
 
 		Main.WIDTH = Integer.parseInt(args[0]);
 		Main.HEIGHT = Integer.parseInt(args[1]);
