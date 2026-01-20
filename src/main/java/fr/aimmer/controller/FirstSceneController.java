d89db397dfe6c30e249790a5f953459d043a0e6f
aimmer <alex.immer@edu.univ-fcomte.fr>
2026-01-20 10:22:23 +0100

    feat: affichage de la video sur la scene

--- src/main/java/fr/aimmer/controller/FirstSceneController.java
+++ src/main/java/fr/aimmer/controller/FirstSceneController.java
@@ -4,26 +4,49 @@ import fr.aimmer.view.GoHomeButton;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
-import static fr.aimmer.Main.HEIGHT;
-import static fr.aimmer.Main.WIDTH;
+import java.net.URL;
 
-public class SettingsController implements Controller
+import static fr.aimmer.Main.*;
+
+public class FirstSceneController implements Controller
 {
-	private final VBox root = new VBox();
-
-	public SettingsController()
-	{
-		root.setSpacing(20);
-		root.getChildren().addAll(
-				new Label("Settings page"),
-				new GoHomeButton()
-		);
-	}
 
 	@Override
 	public Scene get()
 	{
+		MediaView mediaView = getMediaView();
+
+		VBox root = new VBox();
+		root.setSpacing(20);
+		root.getChildren().addAll(
+				new Label("Etape 1 : Rendu video"),
+				mediaView,
+				new GoHomeButton()
+		);
+
 		return new Scene(root, WIDTH, HEIGHT);
+	}
+
+	private MediaView getMediaView()
+	{
+		URL videoUrl = getClass().getResource(FILE_PATH);
+
+		if (videoUrl == null) throw new NullPointerException("< " + FILE_PATH + " > not found");
+
+		Media media = new Media(videoUrl.toExternalForm());
+
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+		MediaView mediaView = new MediaView(mediaPlayer);
+
+		mediaView.setFitWidth(WIDTH * 0.8);
+		mediaView.preserveRatioProperty().set(true);
+		mediaPlayer.play();
+
+		return mediaView;
 	}
 }
