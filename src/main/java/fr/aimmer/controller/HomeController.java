c80044a5a378188d44ed86457f2b1ea5087cd133
aimmer <alex.immer@edu.univ-fcomte.fr>
2026-01-21 18:01:03 +0100

    feat: bouton centrer à l'ecran

--- src/main/java/fr/aimmer/controller/HomeController.java
+++ src/main/java/fr/aimmer/controller/HomeController.java
@@ -1,8 +1,11 @@
 package fr.aimmer.controller;
 
 import fr.aimmer.ui.scene.SceneManager;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 import static fr.aimmer.Main.HEIGHT;
 import static fr.aimmer.Main.WIDTH;
@@ -12,10 +15,15 @@ public class HomeController implements Controller
 	@Override
 	public Scene get()
 	{
-		Button button = new Button("SCENE 1");
-		button.setOnAction(_ ->
-								   SceneManager.getInstance().switchTo("scene:1"));
+		VBox root = new VBox(20);
+		root.setAlignment(Pos.CENTER);
 
-		return new Scene(button, WIDTH, HEIGHT);
+		Button button = new Button("Vidéo encryptée");
+		button.setOnAction(_ -> SceneManager.getInstance().switchTo("scene:1"));
+		button.setPadding(new Insets(10, 40, 10, 40));
+
+		root.getChildren().add(button);
+
+		return new Scene(root, WIDTH, HEIGHT);
 	}
 }
