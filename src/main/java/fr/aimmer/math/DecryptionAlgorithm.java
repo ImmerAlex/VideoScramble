d139453ba1ca685d196aa68291c25bf1ddbe49f2
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2026-01-23 14:37:33 +0100

    feat: avancement methode euclidienne et cleanup pearson

--- src/main/java/fr/aimmer/math/DecryptionAlgorithm.java
+++ src/main/java/fr/aimmer/math/DecryptionAlgorithm.java
@@ -2,21 +2,34 @@ package fr.aimmer.math;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 
 public class DecryptionAlgorithm {
 
 	public static File euclideDecrypt(File encryptedFile) throws IOException {
 		File decryptedFile = null;
+
 		// Charger la vidéo
-		var encryptedVideoCapture = new VideoCapture(encryptedFile.getAbsolutePath());
-		// TODO: Tout faire en fait
+		VideoCapture encryptedVideoCapture = new VideoCapture(encryptedFile.getAbsolutePath());
 
-		return decryptedFile;
+		// Récupérer la première frame de la vidéo
+		Mat firstEncryptedVideoFrame = new Mat();
+		if (!encryptedVideoCapture.read(firstEncryptedVideoFrame)) {
+			throw new RuntimeException("Impossible d'obtenir la première frame de la vidéo");
+		}
+
+		List<Double> distances = new ArrayList<>(firstEncryptedVideoFrame.rows() - 1);
+		for (int i = 0; i < firstEncryptedVideoFrame.rows() - 1; i++) {
+			Mat currentLine = firstEncryptedVideoFrame.row(i);
+			Mat nextLine = firstEncryptedVideoFrame.row(i+1);
+			// distances.add();
+		}
+
+		throw new UnsupportedOperationException("Methode de déchiffrage par Euclide en cours d'implémentation");
 	}
 
-	public static File pearsonDecrypt(File file) {
-		return file;
-	}
 }
